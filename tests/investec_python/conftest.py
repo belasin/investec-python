580b4ae94cb748c264e64b78ebd1f73226fa1a26
Rameez Khan <rameezkhan.sa@gmail.com>
2023-10-18 11:02:02 +0200

    feat: get account balance and transactions

--- tests/investec_python/conftest.py
+++ tests/investec_python/conftest.py
@@ -5,5 +5,5 @@ import investec_python
 
 @pytest.fixture(scope="session")
 def investec_client() -> investec_python.Investec:
-    client = investec_python.Investec(use_sandbox=True)
+    client = investec_python.Investec(use_sandbox=True, debug=True)
     return client
