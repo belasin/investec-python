19173f296aa3086ec985c48bb575c4480840768b
Rameez Khan <rameez@rameezkhan.dev>
2025-08-28 09:05:51 +0200

    feat: add pdm to nix shell

--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
-let 
+let
   pkgs = import <nixpkgs> { };
 in pkgs.mkShell rec {
   name = "investec-python";
   buildInputs = with pkgs; [
     (python311.withPackages (ps: [ps.pip]))
+    pdm
   ];
   shellHook = ''
     export PATH=${pkgs.python311}/bin:$PATH
